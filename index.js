9411659c2031dc4d296e2e94409e312673fb551b
Anatolii Pachkov <anatolii.pachkov@gmail.com>
2018-02-06 21:39:58 +0200

    update server response on OPTION method
    - allow cros domain requests with Authorization header

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ app.use(express.bodyParser());
 
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, X-XSRF-TOKEN');
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,OPTIONS,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization');
   next();
 };
 
